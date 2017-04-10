0f6df0e6032e320a42908920959cd3a3a9e0d93c
mywave76 <mywave.reloaded@yahoo.com>
2017-04-10 22:49:11 +0200

    Update view using a Django templating language

--- books/views.py
+++ books/views.py
@@ -1,6 +1,5 @@
-from django.http import	HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def list_books(request):
-	return HttpResponse("Hello, world!")
+	return render(request, "list.html")
