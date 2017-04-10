663f1fb96a0c4d615cdfd2c68b48df3f0a9d1a81
mywave76 <mywave.reloaded@yahoo.com>
2017-04-10 19:54:31 +0200

    Create custom BookAdmin

--- books/models.py
+++ books/models.py
@@ -16,11 +16,11 @@ class Book(models.Model):
 	def list_authors(self):
 		return ", ".join([author.name for author in self.authors.all()])
 
-    def save(self, *args, **kwargs):
-    	if (self.review and self.date_reviewed is None):
-    		self.date_reviewed = now()
+	def save(self, *args, **kwargs):
+		if (self.review and self.date_reviewed is None):
+			self.date_reviewed = now()
 
-    	super(Book, self).save(*args, **kwargs)
+		super(Book, self).save(*args, **kwargs)
 
 
 class Author(models.Model):
